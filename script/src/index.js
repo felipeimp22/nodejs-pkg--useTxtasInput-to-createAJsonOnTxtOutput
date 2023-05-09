34e99326f86d93e23d0af02ebd2210fe38cd9d00
felipeimp22 <felipeimp22>
2023-05-09 02:29:42 -0300

    reading multiple lines

--- script/src/index.js
+++ script/src/index.js
@@ -45,18 +45,20 @@ const jsonModel = {
 "tipo": "banners_deck"
 }
 
+let rowData = []
+let formatedData;
 
 const fileReadingPath = path.join('..','files', 'example.txt');
 const outputReadingPath = path.join('..','files', 'output.txt');
 
 let splitedData = []
-let rawJson ={}
-let jsonFormated = jsonModel
+// let rawJson ={}
+// let jsonFormated = jsonModel
+
 
 const readFile = async () => {
     try {
         const rawData = await fs.readFile(fileReadingPath, 'utf8');
-
         return rawData
 
     } catch (e) {
@@ -68,11 +70,11 @@ const readFile = async () => {
 const splitDataFnc = async () => {
     try {
         await readFile()
-        let rawSplitedData = `${await readFile()}`.split("\t")
+        let rawSplitedData = `${await readFile()}`.split(/[\t|\n]/)
 
         rawSplitedData.map(e => {
             if (e.includes("\r\n")) {
-                return splitedData.push(e.replace("\r\n", ""))
+                return splitedData.push(e.replace("\r", ""))
             }
             return splitedData.push(e)
         })
@@ -85,47 +87,62 @@ const createRawJson = async () => {
     try {
         await splitDataFnc()
         let obj = {}
-        DTO.map((e,i)=>obj[e]= splitedData[i])
-        rawJson = obj
+        for(let i=0; i<splitedData.length; i++) {     
+            if( i == 22){
+                rowData.push(obj)
+                obj ={}
+                splitedData.splice(0, i) 
+                i = 0;
+            }
+            obj[DTO[i]] = splitedData[i]
+
+              continue; // continue to the next iteration
+          }
     }
     catch (e) {
         console.log(e.message)
         return e.message
     }
 }
+
 
 const createFormatedJson = async() =>{
     try {
         await createRawJson()
-        let pathImagemS3= JSON.parse(jsonFormated.pathImagemS3)
-        pathImagemS3.default = pathImagemS3.default + rawJson["Nome da Imagem"] + "-mobile" + rawJson["Extensão"]
+        let assemblingObj = jsonModel
+        let bindObj = ''
 
-        let defaultMetaData = JSON.parse(jsonFormated.defaultMetaData)
-        defaultMetaData["url_img"] = defaultMetaData["url_img"] + rawJson["Nome da Imagem"]
-        defaultMetaData["ext_img"] = rawJson["Extensão"]
-        defaultMetaData["TAG"] = rawJson["TAG"]
 
-        let defaultUrlRedirect =  JSON.parse(jsonFormated.defaultUrlRedirect)
-        defaultUrlRedirect.default = rawJson["Default"]
-        defaultUrlRedirect["MCLA"] = rawJson["Site"]
-        defaultUrlRedirect["MCAP"] = rawJson["App"]
+        rowData.forEach((e,i, arr)=>{
+            let pathImagemS3= JSON.parse(assemblingObj.pathImagemS3)
+            pathImagemS3.default = pathImagemS3.default + e["Nome da Imagem"] + "-mobile" + e["Extensão"]
 
-        jsonFormated.idcampanha = rawJson["ID"]
-        jsonFormated.ativo = Boolean(rawJson["Ativo"])
-        jsonFormated.codCampanha = rawJson["Código"]
-        jsonFormated.descricao = rawJson["Descrição"]
-        jsonFormated.segmento = rawJson["Segmento"]
-        jsonFormated.prioridade = rawJson["Prioridade"]
-        jsonFormated.textoPrincipal = rawJson["Texto Principal"]
-        jsonFormated.metodo = rawJson["Metodo"]
-        jsonFormated.nomeLambda = rawJson["Lambda"]
-        // jsonFormated.pathImagemS3 = `{\"default\": \"${pathImagemS3.default +  rawJson["Nome da Imagem"] + "-mobile" + rawJson["Extensão"]}\"}`
-        jsonFormated.pathImagemS3 = JSON.stringify(pathImagemS3)
-        jsonFormated.defaultMetaData = JSON.stringify(defaultMetaData)
-        jsonFormated.defaultUrlRedirect = JSON.stringify(defaultUrlRedirect)
+            let defaultMetaData = JSON.parse(assemblingObj.defaultMetaData)
+            defaultMetaData["url_img"] = defaultMetaData["url_img"] + e["Nome da Imagem"]
+            defaultMetaData["ext_img"] = e["Extensão"]
+            defaultMetaData["TAG"] = e["TAG"]
 
-        // console.log(JSON.stringify(jsonFormated))
-        // console.log(jsonFormated)
+            let defaultUrlRedirect =  JSON.parse(assemblingObj.defaultUrlRedirect)
+            defaultUrlRedirect.default = e["Default"]
+            defaultUrlRedirect["MCLA"] = e["Site"]
+            defaultUrlRedirect["MCAP"] = e["App"]
+
+            assemblingObj.idcampanha = e["ID"]
+            assemblingObj.ativo = Boolean(e["Ativo"])
+            assemblingObj.codCampanha = e["Código"]
+            assemblingObj.descricao = e["Descrição"]
+            assemblingObj.segmento = e["Segmento"]
+            assemblingObj.prioridade = e["Prioridade"]
+            assemblingObj.textoPrincipal = e["Texto Principal"]
+            assemblingObj.metodo = e["Metodo"]
+            assemblingObj.nomeLambda = e["Lambda"]
+            assemblingObj.pathImagemS3 = JSON.stringify(pathImagemS3)
+            assemblingObj.defaultMetaData = JSON.stringify(defaultMetaData)
+            assemblingObj.defaultUrlRedirect = JSON.stringify(defaultUrlRedirect)
+
+            bindObj += i != 0 && i != arr.length ? "," + JSON.stringify(assemblingObj) : JSON.stringify(assemblingObj)
+        })
+        formatedData = JSON.parse("[" + bindObj + "]")
     }
     catch (e) {
         console.log(e.message)
@@ -133,36 +150,24 @@ const createFormatedJson = async() =>{
     } 
 }
 
-const checkIfNeedCommaOnOutput = async() => {
-    try{
-    const rawOutput = await fs.readFile(outputReadingPath, 'utf8');
-    return rawOutput.length > 0 && true
-    } catch (e) {
-        return e.message
-    } 
-}
-
-const insertCommaOnOutput = async() =>{
-    try{
-
-       await fs.appendFile(outputReadingPath,", \n" )
-
-    }catch (e) {
-        return e.message
-    } 
-
-}
 
 const createFile = async() =>{
     try{
         await createFormatedJson()
-        const commaValidation = await checkIfNeedCommaOnOutput()
-        if(commaValidation){
-           await insertCommaOnOutput() 
-        }
-        const formatedOutput = JSON.stringify(jsonFormated, null, 2)
+        // const commaValidation = await checkIfNeedCommaOnOutput()
+        // if(commaValidation){
+        //    await insertCommaOnOutput() 
+        // }
 
-       await fs.appendFile(outputReadingPath,formatedOutput )
+        //caso necessario tirar os [] do arquivo:
+        // formatedData.forEach()
+        // para cada obj usar appendFile e voltar com o commaValidation
+
+        const formatedOutput = JSON.stringify(formatedData, null, 2)
+
+    //    await fs.appendFile(outputReadingPath,formatedOutput.substring(1, formatedOutput.length - 1).slice(0, -1) )
+    await fs.appendFile(outputReadingPath,formatedOutput )
+
 
     }catch (e) {
         console.log(e.message)
@@ -179,3 +184,21 @@ const createFile = async() =>{
     console.timeEnd("Processing");
 
 })()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
